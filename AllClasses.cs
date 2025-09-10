e4a37db57a9821ab728ee0f549d4654fef8a77b0
MBen <manbeniamin15@gmail.com>
2025-09-10 09:25:43 +0300

    First steps (Base structor)

---
+++ AllClasses.cs
@@ -0,0 +1,51 @@
+﻿/*Description:
+ 
+ 
+ */
+
+/*TO-DO:
+- Make a base structure. (Done)
+- 
+ 
+ */
+
+
+interface iFilehandler
+{
+
+}
+
+interface iCalculation
+{
+
+}
+
+interface iUserInput
+{
+
+}
+
+interface iUserOutput
+{
+
+}
+
+public class FileHandler:iFilehandler
+{
+
+}
+
+public class AverageCalculator:iCalculation
+{
+
+}
+
+public class UserInput:iUserInput
+{
+
+}
+
+public class UserOutput:iUserOutput
+{
+
+}
